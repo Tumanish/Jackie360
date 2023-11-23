cc0ec30f081fd3091d61a297c4e14b5a1b6cda9f
Tumanish <59100693+Tumanish@users.noreply.github.com>
2023-11-23 16:37:07 +0300

    green

--- script.js
+++ script.js
@@ -14,3 +14,20 @@ let Options = {
 };
 
 new Swiper('.swiper', Options);
+
+
+
+
+function starClick(element) {
+	let empty = document.getElementById("section-main-video__star--img1");
+	let full = document.getElementById("section-main-video__star--img2");
+
+	if (empty.classList.contains("main-video__star--hide")) {
+		empty.classList.remove("main-video__star--hide");
+		full.classList.add("main-video__star--hide");
+	} else {
+		empty.classList.add("main-video__star--hide");
+		full.classList.remove("main-video__star--hide");
+	}
+
+}
